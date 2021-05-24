17395133511e556f060883cacff9396028fd9af8
s-elshot <74968337+s-elshot@users.noreply.github.com>
2021-05-24 20:08:46 +0200

    created authContext and authState

---
+++ src/context/AuthContext.js
@@ -0,0 +1,59 @@
+import React, {createContext, useState} from 'react'
+
+//  CONTEXT
+// 1. context maken met createContext
+// 2.context provider-component bouwen met daarin:
+// - het echte AuthContext.Provider component
+// - geef eem data object mee in de provider
+// - state
+// 3. Wikkelen de provider om <App> heen in index.js
+
+// AUTHENTICATIE
+
+// 0. Bedenk welke data je in de context beschikbaar moet stellen en
+// maak raamwerk voor alle informatie die in de context moet komen
+// (login/logout state)
+// 1. state maken voor gebruikersdata en lege functies
+// 2. Inlogfunctie: Proces inloggen (JWT token in local storage zetten
+// en gebruikersdata opslaan in de context) in de provider regelen
+//3. Uitlogfunctie: jwt ui local storage halen en context leeghalen
+//4. Implementeren dat bij refresh wordt gecheckt of er nog een jwt token is
+// en zo ja, gebruikersdata ophalen.
+
+export const AuthContext = createContext({});
+
+function AuthContextProvider({children}) {
+
+
+    // state gebruikersdata
+    const [authState, setAuthState] = useState({
+        user:null,
+    })
+
+    // inlogfunctie
+    function logIn(){
+        console.log("logIn!")
+    }
+    // uitlogfunctie
+
+    function logOut(){
+        console.log("logOut!")
+    }
+
+    // omdat authstate een object is en we nog steeds gebruik willen
+    // maken van de automatische state-update, zullen we de authState "spreaden"
+    const data = {
+        ...authState,
+        logIn: logOut,
+        logOut: logOut,
+
+    }
+    return(
+        <AuthContext.Provider value={data}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
+
+export default AuthContextProvider
--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from "react-router-dom";
-
+import AuthContextProvider from "./context/AuthContext";
 
 
 ReactDOM.render(
-
     <React.StrictMode>
         <Router>
-            <App/>
+            <AuthContextProvider>
+                <App/>
+            </AuthContextProvider>
         </Router>
     </React.StrictMode>,
     document.getElementById('root')
